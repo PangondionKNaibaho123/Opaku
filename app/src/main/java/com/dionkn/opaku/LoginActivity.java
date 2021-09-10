fcbbc48431864b04048f963ab0a59efe26a454ce
PangondionKNaibaho123 <dionkurniawan130@gmail.com>
2021-09-10 18:05:44 +0700

    make an intent to register activity

--- app/src/main/java/com/dionkn/opaku/LoginActivity.java
+++ app/src/main/java/com/dionkn/opaku/LoginActivity.java
@@ -4,14 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+    private Button buttonRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
         getSupportActionBar().hide();
+
+        buttonRegister = (Button) findViewById(R.id.button_register);
+
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(registerIntent);
+            }
+        });
+
 
     }
 
